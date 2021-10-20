5327725802d1ba2ff05d4cad0f0dbc541770e197
Manoj <manojk93002@gmail.com>
2021-10-20 11:08:43 +0530

    commited

--- shop/views.py
+++ shop/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from E_Commerce.settings import MEDIA_ROOT
 from shop.models import Product
+from shop.models import Contact
 from math import ceil
 
 def index(request):
-    products= Product.objects.all()
     allProds=[]
     catprods= Product.objects.values('category')
     cats= { item['category'] for item in catprods}
@@ -20,3 +20,22 @@ def index(request):
 
 def about(request):
     return render(request,"shop/about.html")
+
+def tracker(request):
+    return render(request,"shop/tracker.html")
+
+def contact(request):
+    if(request.method=="POST"):
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phoneNumber=request.POST.get('phoneNumber')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name,email=email,phoneNumber=phoneNumber,desc=desc)
+        contact.save()
+    return render(request,"shop/contact.html")
+
+def productView(request,myid):
+    products=Product.objects.filter(id=myid)
+    print(products)
+    params={'product':products[0]}
+    return render(request,"shop/productView.html",params)
